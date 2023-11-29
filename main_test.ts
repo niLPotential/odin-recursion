44822b06e6dc36cdffe2ab928c0a9f3ef3a24d09
niLPotential <niLPotential@proton.me>
2023-11-29 16:56:09 +0900

    Add fib test up to 20

--- main_test.ts
+++ main_test.ts
@@ -5,10 +5,24 @@ Deno.test(function fibsTest() {
   assertEquals(fibs(1), [0]);
   assertEquals(fibs(2), [0, 1]);
   assertEquals(fibs(8), [0, 1, 1, 2, 3, 5, 8, 13]);
+  assertEquals(
+    fibs(20),
+    [
+      0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597,
+      2584, 4181,
+    ]
+  );
 });
 
 Deno.test(function fibsRecTest() {
   assertEquals(fibsRec(1), [0]);
   assertEquals(fibsRec(2), [0, 1]);
   assertEquals(fibsRec(8), [0, 1, 1, 2, 3, 5, 8, 13]);
+  assertEquals(
+    fibsRec(20),
+    [
+      0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597,
+      2584, 4181,
+    ]
+  );
 });
